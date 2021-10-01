24315c9df14aa3528847eb5f0048259b096a97c3
gulnara <gulnara.ibragimova@sms-digital.com>
2021-10-01 20:55:05 +0200

    Extract query to file

--- postgres_to_es/etl.py
+++ postgres_to_es/etl.py
@@ -13,20 +13,26 @@ class Etl:
         dsn = dict(self.config.film_work_pg.dsn)
         self.pg_conn = psycopg2.connect(**dsn, cursor_factory=DictCursor)
         self.state = State(JsonFileStorage(self.config.film_work_pg.state_file_path))
+        self._fetch_query = None
 
     def run(self):
-        extracted = self.extract_records()
+        extracted = self.extract()
         transformed = self.transform(extracted)
         self.load(transformed)
 
     def _get_update_time(self, default_value=datetime.datetime(1970, 1, 1)):
         return self.state.get_state("last_updated_at") or default_value
 
-    def extract_records(self):
+    def _get_guery(self):
+        if not self._fetch_query:
+            with open("postgres_query.sql", "r") as query_file:
+                self._fetch_query = query_file.read()
+        return self._fetch_query
+
+    def extract(self):
+        query = self._get_guery()
         update_time = self._get_update_time()
         cursor = self.pg_conn.cursor()
-        with open("postgres_query.sql", "r") as query_file:
-            query = query_file.read()
         cursor.execute(query, (update_time,))
         res = [item for item in cursor]
         return res
